e481889ac24aec2d0de9054e4726e4253b65fd57
Addison <addison148@gmail.com>
2022-01-08 14:18:07 -0800

    add js & linkedin button

---
+++ script.js
@@ -0,0 +1,23 @@
+let doc = document.getElementById("textWrite");
+doc.innerHTML = ">&#8203;";
+let i = 0;
+let str = "Hi there!";
+
+const typeTimer = setInterval(function () {
+    i = i + 1;
+    doc.innerHTML = "> " + str.slice(0, i) + "|";
+    if (i === str.length) {
+        clearInterval(typeTimer);
+        doc.innerHTML = "> " + str;
+        i = 0;
+        setInterval(function () {
+            if (i === 0) {
+                doc.innerHTML = "> " + str + "|"
+                i = 1;
+            } else {
+                doc.innerHTML = "> " + str
+                i = 0;
+            };
+        }, 500);
+    };
+}, 250);
